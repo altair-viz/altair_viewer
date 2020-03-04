f842e00dfd9348b54b901367e4b2dd9c62fca24b
Jake VanderPlas <jakevdp@google.com>
2020-03-03 20:08:22 -0800

    MAINT: bump version to 0.2.1

--- altair_viewer/__init__.py
+++ altair_viewer/__init__.py
@@ -1,6 +1,6 @@
 """Altair viewer provides offline viewing for Altair charts."""
 
-__version__ = "0.3.0.dev0"
+__version__ = "0.2.1"
 __all__ = ["ChartViewer", "display", "render", "show", "get_bundled_script"]
 
 from altair_viewer._viewer import ChartViewer
