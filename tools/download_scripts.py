e58dac4524733bb13873e2b5791f013493c10c76
Jake VanderPlas <jakevdp@google.com>
2021-11-06 05:27:12 -0700

    MAINT: add vega 5.21.0 and vega-embed 6.20.0

--- tools/download_scripts.py
+++ tools/download_scripts.py
@@ -3,9 +3,9 @@ import os
 from urllib.request import urlretrieve
 
 PACKAGES = {
-    "vega": ["5.9.2", "5.10.1"],
-    "vega-lite": ["4.0.2", "4.8.1"],
-    "vega-embed": ["6.2.2", "6.5.2"],
+    "vega": ["5.9.2", "5.10.1", "5.21.0"],
+    "vega-lite": ["4.0.2", "4.8.1", "4.17.0"],
+    "vega-embed": ["6.2.2", "6.5.2", "6.20.0"],
 }
 
 OUTDIR = os.path.join(os.path.dirname(__file__), "..", "altair_viewer", "scripts")
