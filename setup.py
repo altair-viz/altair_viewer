14b50184af5a1ea90611a1e72d32922b06bb7521
mattijn <mattijn@gmail.com>
2022-01-12 14:48:53 +0100

    set min version to python 3.7, same as altair

--- setup.py
+++ setup.py
@@ -69,9 +69,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=get_install_requirements("requirements.txt"),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     entry_points={
-        "altair.vegalite.v4.renderer": ["altair_viewer=altair_viewer:render"],
+        "altair.vegalite.v5.renderer": ["altair_viewer=altair_viewer:render"],
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -79,8 +79,9 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
