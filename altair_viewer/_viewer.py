2d3a0df3077ac8d44c87d94c641e8738d322b847
mattijn <mattijn@gmail.com>
2022-01-13 20:08:54 +0100

    revert viewer changes

--- altair_viewer/_viewer.py
+++ altair_viewer/_viewer.py
@@ -69,32 +69,22 @@ HTML = """
 INLINE_HTML = r"""
 <div id="{output_div}"></div>
 <script type="text/javascript">
-  var VEGA_DEBUG = (typeof VEGA_DEBUG == "undefined") ? {{}} : VEGA_DEBUG;
-  (function(spec, embedOpt){{
-    let outputDiv = document.currentScript.previousElementSibling;
-    if (outputDiv.id !== "{output_div}") {{
-      outputDiv = document.getElementById("{output_div}");
-    }}
-    const paths = {{
+  (function(spec, embedOpt) {{
+    const outputDiv = document.getElementById("{output_div}");
+    const urls = {{
       "vega": "{vega_url}",
       "vega-lite": "{vegalite_url}",
       "vega-embed": "{vegaembed_url}",
     }};
-    function maybeLoadScript(lib, version) {{
-      var key = `${{lib.replace("-", "")}}_version`;
-      return (VEGA_DEBUG[key] == version) ?
-        Promise.resolve(paths[lib]) :
-        new Promise(function(resolve, reject) {{
-          var s = document.createElement('script');
-          document.getElementsByTagName("head")[0].appendChild(s);
-          s.async = true;
-          s.onload = () => {{
-            VEGA_DEBUG[key] = version;
-            return resolve(paths[lib]);
-          }};
-          s.onerror = () => reject(`Error loading script: ${{paths[lib]}}`);
-          s.src = paths[lib];
-        }});
+    function loadScript(lib) {{
+      return new Promise(function(resolve, reject) {{
+        var s = document.createElement('script');
+        s.src = urls[lib];
+        s.async = true;
+        s.onload = () => resolve(urls[lib]);
+        s.onerror = () => reject(`Error loading script: ${{urls[lib]}}`);
+        document.getElementsByTagName("head")[0].appendChild(s);
+      }});
     }}
     function showError(err) {{
       outputDiv.innerHTML = `<div class="error" style="color:red;">${{err}}</div>`;
@@ -104,15 +94,23 @@ INLINE_HTML = r"""
       vegaEmbed(outputDiv, spec, embedOpt)
         .catch(err => showError(`Javascript Error: ${{err.message}}<br>This usually means there's a typo in your chart specification. See the javascript console for the full traceback.`));
     }}
+
     if(typeof define === "function" && define.amd) {{
-      requirejs.config({{paths}});
-      require(["vega-embed"], displayChart, err => showError(`Error loading script: ${{err.message}}`));
+        // requirejs paths need '.js' extension stripped.
+        const paths = Object.keys(urls).reduce(function(paths, package) {{
+            paths[package] = urls[package].replace(/\.js$/, "");
+            return paths
+        }}, {{}})
+        requirejs.config({{paths}});
+        require(["vega-embed"], displayChart, err => showError(`Error loading script: ${{err.message}}`));
+    }} else if (typeof vegaEmbed === "function") {{
+        displayChart(vegaEmbed);
     }} else {{
-      maybeLoadScript("vega", "{{vega_version}}")
-        .then(() => maybeLoadScript("vega-lite", "{{vegalite_version}}"))
-        .then(() => maybeLoadScript("vega-embed", "{{vegaembed_version}}"))
-        .catch(showError)
-        .then(() => displayChart(vegaEmbed));
+        loadScript("vega")
+            .then(() => loadScript("vega-lite"))
+            .then(() => loadScript("vega-embed"))
+            .catch(showError)
+            .then(() => displayChart(vegaEmbed));
     }}
   }})({spec}, {embedOpt});
 </script>
